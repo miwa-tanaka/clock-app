c446cdd4e46d9f85311c976d0b7792cd661fe5fe
Miwa <miwa.tnk9675@gmail.com>
2022-02-07 08:47:46 +0900

    added global scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    { src: '~/assets/scss/common.scss' },
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -39,6 +40,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
