034c0fac933dc51879865eac8b48f38012c8eac2
Miwa <miwa.tnk9675@gmail.com>
2022-02-07 08:48:10 +0900

    created clock file

---
+++ components/Clock.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div style="background: pink">
+      <h1>hello world</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        timeZone: null
+      }
+     },
+     created () {
+       this.hoge()
+     },
+     methods: {
+       hoge () {
+         console.log('ああああああ')
+
+       }
+     }
+  }
+</script>
