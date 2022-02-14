49a5167d67e176bfbffc1e786218cb205a091e9a
Miwa <miwa.tnk9675@gmail.com>
2022-02-14 16:17:13 +0900

    added base

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'clock-app/favicon.ico' }
     ]
   },
 
@@ -48,9 +48,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
-  ,
+  },
+
   env: {
     API_KEY
+  },
+
+  router: {
+    base: '/clock-app/'
   }
 }
